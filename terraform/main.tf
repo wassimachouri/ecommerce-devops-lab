681ae4793f9a9c46a38c42edcb2971a8ac375308
wassimachouri <wassimachouri22@gmail.com>
2026-05-21 21:29:12 +0100

    add output ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,17 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.rt.id
 }
 
 resource "aws_security_group" "sg" {
   name   = "devops-sg"
-  vpc_id = aws_vpc.main.id
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -57,12 +39,11 @@ resource "aws_instance" "web" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
   key_name               = "vockey"
-  subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg.id]
-
-  associate_public_ip_address = true
+  subnet_id = data.aws_subnets.default.ids[0]
 
   tags = {
     Name = "devops-ec2"
   }
 }
+
